905980b83f8d10bb496e58735d138d7fe03dfd0d
Souroop Pattanaik <sourooppatnaik@gmail.com>
2025-07-17 20:10:59 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,32 @@
+package learn;
+interface Flyable{
+	void fly();
+}
+interface Swimmable{
+	void swim();
+}
+class Duck implements Flyable,Swimmable{
+	String name;
+	public Duck(String name) {
+		this.name=name;
+	}
+	public void fly() {
+		System.out.println(name+" is flying!!");
+	}
+	public void swim() {
+		System.out.println(name+" is Swimming!!");
+	}
+	public void displayDetails() {
+		System.out.println("Duck Name: "+name);
+		System.out.println("Abilities: ");
+		fly();
+		swim();
+	}
+}
+public class Eight {
+	public static void main(String[]args) {
+		Duck duck=new Duck("Raghu");
+		System.out.println("Duck Details: ");
+		duck.displayDetails();
+	}
+}
